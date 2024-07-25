1210b09af177b3c0d6e7d3202210bd55f0f5da63
softontechsolution <softontecsolution@gmail.com>
2024-07-25 01:51:28 +0000

    file corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-"""
-app
-"""
-
-from flask import Flask, jsonify
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-from os import getenv
-
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
